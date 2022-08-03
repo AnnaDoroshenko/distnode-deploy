ee247b036425b335bf1f5908e562f87abeba79d3
AnnaDoroshenko <annadoroshenko03@gmail.com>
2022-08-03 22:52:40 +0300

    Application files

---
+++ server.js
@@ -0,0 +1,17 @@
+const server = require('fastify')();
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 8000;
+const Recipe = require('./recipe.js');
+
+server.get('/', async (req, res) => {
+  return "Hello from Distributed Node.js!";
+});
+server.get('/recipes/:id', async (req, res) => {
+  const recipe = new Recipe(req.params.id);
+  await recipe.hydrate();
+  return recipe;
+});
+
+server.listen(PORT, HOST, (err, address) => {
+  console.log(`Server running at ${address}`);
+});
